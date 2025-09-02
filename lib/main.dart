8fccfbb18b20bcde02b07d02321254e14b593c1e
ahmedadelattya <ahmedadelattya2@gmail.com>
2025-09-02 15:40:00 +0300

    Quizzler App

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,168 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
 
-void main() {
-  runApp(const XylophoneApp());
-}
+QuizBrain quizBrain = QuizBrain();
 
-class XylophoneApp
-    extends StatelessWidget {
-  const XylophoneApp({super.key});
+void main() => runApp(Quizzler());
 
-  void playSound(
-    int soundNumber,
-  ) async {
-    final player = AudioPlayer();
-    await player.play(
-      AssetSource(
-        'audio/note$soundNumber.wav',
-      ),
-    );
-  }
-
-  // Reusable key builder
-  Widget buildKey({
-    required Color color,
-    required int soundNumber,
-  }) {
-    return Expanded(
-      child: TextButton(
-        onPressed: () {
-          playSound(soundNumber);
-        },
-        style: TextButton.styleFrom(
-          backgroundColor: color,
-          shape: RoundedRectangleBorder(
-            borderRadius:
-                BorderRadius.zero,
-          ),
-        ),
-        child: const SizedBox.shrink(),
-      ),
-    );
-  }
-
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor:
+            Colors.grey.shade900,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment
-                    .stretch,
-            children: [
-              buildKey(
-                color: Colors.red,
-                soundNumber: 1,
-              ),
-              buildKey(
-                color: Colors.orange,
-                soundNumber: 2,
-              ),
-              buildKey(
-                color: Colors.yellow,
-                soundNumber: 3,
-              ),
-              buildKey(
-                color: Colors.green,
-                soundNumber: 4,
-              ),
-              buildKey(
-                color: Colors.teal,
-                soundNumber: 5,
-              ),
-              buildKey(
-                color: Colors.blue,
-                soundNumber: 6,
-              ),
-              buildKey(
-                color: Colors.purple,
-                soundNumber: 7,
-              ),
-            ],
+          child: Padding(
+            padding:
+                EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                ),
+            child: QuizPage(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() =>
+      _QuizPageState();
+}
+
+class _QuizPageState
+    extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  int correctAnswers = 0;
+
+  void checkAnswer(
+    bool userPickedAnswer,
+  ) {
+    if (quizBrain.isFinished()) {
+      Alert(
+        context: context,
+        title: 'Finished!',
+        desc:
+            'Your score is $correctAnswers / ${quizBrain.getTotalQuestions()}',
+      ).show();
+
+      setState(() {
+        quizBrain.reset();
+        scoreKeeper = [];
+        correctAnswers = 0;
+      });
+    } else {
+      setState(() {
+        if (quizBrain
+                .getQuestionAnswer() ==
+            userPickedAnswer) {
+          scoreKeeper.add(
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
+          correctAnswers++;
+        } else {
+          scoreKeeper.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
+        }
+        quizBrain.nextQuestion();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment:
+          MainAxisAlignment
+              .spaceBetween,
+      crossAxisAlignment:
+          CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(
+              10.0,
+            ),
+            child: Center(
+              child: Text(
+                quizBrain
+                    .getQuestionText(),
+                textAlign:
+                    TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(
+              15.0,
+            ),
+            child: TextButton(
+              style:
+                  TextButton.styleFrom(
+                    backgroundColor:
+                        Colors.green,
+                    foregroundColor:
+                        Colors.white,
+                    textStyle:
+                        TextStyle(
+                          fontSize:
+                              20.0,
+                        ),
+                  ),
+              child: Text('True'),
+              onPressed: () {
+                // The user picked true
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(
+              15.0,
+            ),
+            child: TextButton(
+              style:
+                  TextButton.styleFrom(
+                    backgroundColor:
+                        Colors.red,
+                    foregroundColor:
+                        Colors.white,
+                    textStyle:
+                        TextStyle(
+                          fontSize:
+                              20.0,
+                        ),
+                  ),
+              child: Text('False'),
+              onPressed: () {
+                // The user picked false.
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+
+        Row(children: scoreKeeper),
+      ],
     );
   }
 }
